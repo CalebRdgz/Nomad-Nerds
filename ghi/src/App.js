fb1ffd5e3361e2a1e02207b156023202d8d6faf4
Arthur <arthurcro17@yahoo.com>
2022-07-14 19:31:48 -0400

    fixed merge errors

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Test from './test_file';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Test /> 
-      </header>
-    </div>
-  );
-=======
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
@@ -74,6 +48,5 @@ class App extends React.Component {
       </BrowserRouter>
     )
   }
->>>>>>> main
 }
 export default App;
