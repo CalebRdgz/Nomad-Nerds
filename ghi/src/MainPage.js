31918f1329e5b5441ade530d217a974120af852a
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-07-14 20:20:08 -0700

    Changed a few things in front-end

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,12 +1,12 @@
-import background from './SF.png';
+import background from './images/SF.png';
 
 function MainPage() {
     return (
     <div className="wrapper" style={{ backgroundImage: `url(${background})`}}>
       <div className="px-5 py-5 my-5 text-center">
         <h1 className="display-5 fw-bold">Nomad-Nerds</h1>
-        <div className="col-lg-4 mx-auto">
-          <p className="lead mb-4">
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-30 pb-5">
             Connecting you to your favorite activities and locations!
           </p>
         </div>
