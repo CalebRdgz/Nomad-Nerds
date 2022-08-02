f05dcc8f0e80fe0d99a9b294f910891bee4b0a52
Caleb <calebrod1@yahoo.com>
2022-08-02 17:48:31 -0500

    update

--- ghi/src/Carousel.js
+++ ghi/src/Carousel.js
@@ -8,19 +8,18 @@ import CitySearch from './searchByCity/citySearch';
 function CarouselFade() {
   return (
     <Carousel fade className="carousel" style={{zIndex: -1}}>
-        
-        <Carousel.Item>
-          <Carousel.Caption>
-            <div className="px-5 py-5 my-5 text-center">
-              <h1 className="display-5 fw-bold text-on-image">FIND YOUR NEXT ADVENTURE</h1>
-              </div>
-          </Carousel.Caption>          
-          <img
-            className="d-block" style={{overflow: "hidden"}}
-            src={ image } 
-            alt="First slide"
-          />
-        </Carousel.Item>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <div className="px-5 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold text-on-image">FIND YOUR NEXT ADVENTURE</h1>
+            </div>
+        </Carousel.Caption>          
+        <img
+          className="d-block" style={{overflow: "hidden"}}
+          src={ image } 
+          alt="First slide"
+        />
+      </Carousel.Item>
 
       <Carousel.Item>
         <Carousel.Caption>
@@ -47,7 +46,6 @@ function CarouselFade() {
           src={ image3 }
           alt="Third slide"
         />
-
       </Carousel.Item>
     </Carousel>
   );
