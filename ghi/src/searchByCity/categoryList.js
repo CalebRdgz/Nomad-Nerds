4d64e0b6d1d5efe407bcb53554c83e8ac5bc5134
Emma Oh <youngeunyou@gmail.com>
2022-08-05 12:20:38 -0700

    updated list

--- ghi/src/searchByCity/categoryList.js
+++ ghi/src/searchByCity/categoryList.js
@@ -16,7 +16,7 @@ function CategoryList() {
     const [businesses, setBusinesses] = useState([]);
     const [business_id, setBusiness_id] = useState('');
     const [businessesLoading, setBusinessesLoading] = useState(true)
-    const [categoriesLoading, setCategoriesLoading]  = useState(true)
+    const [categoriesLoading, setCategoriesLoading] = useState(true)
     const [favorites, setFavorites] = useState([]);
     const { token } = useAuthContext();
     const city = (location.state.city.city).replace(/ /g, '%20');
@@ -58,16 +58,17 @@ function CategoryList() {
         const response = await fetch(categories_url, fetchConfig);
         if (response.ok) {
             const data = await response.json();
-            setCategories(data["categories"])}
-            setCategoriesLoading(false)        
+            setCategories(data["categories"])
+        }
+        setCategoriesLoading(false)
     }
-    
+
     function fetchBusinesses(category, city) {
         const fetchConfig = {
             method: "get",
             headers: {
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin":"*",
+                "Access-Control-Allow-Origin": "*",
             },
         };
         const url = `${process.env.REACT_APP_API_YELP}/api-yelp/businesses/list?category=${category}&location=${cityAndState}&quantity=1`;
@@ -79,14 +80,14 @@ function CategoryList() {
             Promise.all(categories.slice(0, 5)
                 .map(category => fetchBusinesses(category[0], city)
                     .then(res => res.json())
-                    .then(data => ({[category[1]]: data}))))
-                .then(data => (setBusinessesLoading(false), setBusinesses(data)))
+                    .then(data => ({ [category[1]]: data }))))
+                .then(data => (setCategoriesLoading(false), setBusinesses(data)))
         }
     }
 
     async function addFavorite(id) {
         const url = `${process.env.REACT_APP_USER}/user/favorites/`
-        let content = {business_id: id}
+        let content = { business_id: id }
         const fetchConfig = {
             credentials: "include",
             method: "post",
@@ -108,7 +109,7 @@ function CategoryList() {
                 navigate('/user/login/');
             } else {
             }
-            
+
         }
     }
 
@@ -145,19 +146,19 @@ function CategoryList() {
     if (categoriesLoading === false && categories.length === 0 || businessesLoading === false && businesses.length === 0) {
         return (
             <div className="text-center">
-                <img src = {no_info} style={{ height: 400, marginTop: 100}} />
+                <img src={no_info} style={{ height: 400, marginTop: 100 }} />
                 {console.log('city', location.state.city.city)}
                 <h1>Can't find any businesses in {location.state.city.city}</h1>
-                <p style={{ marginBottom: 250 }}className="large fw-bold mt-2 pt-1">Back to <a href="/"
-                  className="link-danger">Home</a></p>
+                <p style={{ marginBottom: 250 }} className="large fw-bold mt-2 pt-1">Back to <a href="/"
+                    className="link-danger">Home</a></p>
             </div>
         )
-    } 
+    }
 
     else if (businessesLoading === true) {
         return (
             <div className="text-center">
-                <img src = "https://theimaa.com.au/wp-content/uploads/2022/06/IMAA_Plan_Around_Globe_Gif_one.gif" style={{ height: 350, marginTop: 100, marginBottom: 30}}/>
+                <img src="https://theimaa.com.au/wp-content/uploads/2022/06/IMAA_Plan_Around_Globe_Gif_one.gif" style={{ height: 350, marginTop: 100, marginBottom: 30 }} />
                 <h1 style={{ marginBottom: 100 }}>Loading...</h1>
             </div>
         )
@@ -166,34 +167,34 @@ function CategoryList() {
     return (
         <ul>
             {businesses.map((business, index) => (
-            <div key={index}>              
-                <Container className="container-fluid">
-                <h1 className="card-title" style={{fontFamily: "papyrus", fontWeight:"bold", padding:20, paddingTop: 90}}>{Object.keys(business)}</h1>
-                <Row className="flex-nowrap flex-row" style={{overflowX: "scroll"}}>
-                  {Object.values(business)[0].slice(0,15).map((store, idx) => (
-                        <Col key={idx} className="col-3">
-                        <Card style={{width: "18rem"}}>                           
-                            <Card.Img variant="top" src={store.image_url} height={250} />
-                            <Card.Body>
-                            <Card.Title style={{fontWeight: "bold"}}>{store.name}</Card.Title>
-                                <Card.Text>
-                                    {store.location.display_address[0]}<br />
-                                    {store.location.display_address[1]}<br />
-                                    {store.location.display_address[2]}<br />
-                                    {store.price? `Price: ${store.price}`: ''}<br /> 
-                                    Rating: {store.rating}
-                                    <Button variant="light"  style={{float: "right"}}>
-                                    {favorites.includes(store.id) ?  <AiFillHeart style={{color: "red", size:'2em'}} onClick={() => deleteFavorite(store.id)}/> : <AiOutlineHeart onClick={() => addFavorite(store.id)}/>}
-                                     </Button>
-                                </Card.Text>                               
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                ))}
-                </Row>
-                </Container>
-            </div>
-                            
+                <div key={index}>
+                    <Container className="container-fluid">
+                        <h1 className="card-title" style={{ fontFamily: "papyrus", fontWeight: "bold", padding: 20, paddingTop: 90 }}>{Object.keys(business)}</h1>
+                        <Row className="flex-nowrap flex-row" style={{ overflowX: "scroll" }}>
+                            {Object.values(business)[0].slice(0, 15).map((store, idx) => (
+                                <Col key={idx} className="col-3">
+                                    <Card style={{ width: "18rem" }}>
+                                        <Card.Img variant="top" src={store.image_url} height={250} />
+                                        <Card.Body>
+                                            <Card.Title style={{ fontWeight: "bold" }}>{store.name}</Card.Title>
+                                            <Card.Text>
+                                                {store.location.display_address[0]}<br />
+                                                {store.location.display_address[1]}<br />
+                                                {store.location.display_address[2]}<br />
+                                                {store.price ? `Price: ${store.price}` : ''}<br />
+                                                Rating: {store.rating}
+                                                <Button variant="light" style={{ float: "right" }}>
+                                                    {favorites.includes(store.id) ? <AiFillHeart style={{ color: "red", size: '2em' }} onClick={() => deleteFavorite(store.id)} /> : <AiOutlineHeart onClick={() => addFavorite(store.id)} />}
+                                                </Button>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </div>
+
             ))}
         </ul>
     )
